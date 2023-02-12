9d7680180554aef35e0da3ee8312d6074fb45237
Nathan Watkins <nathancwatkins23@gmail.com>
2023-02-11 16:49:48 -0800

    first draft

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,93 @@
 import Image from 'next/image'
+import ReactPlayer from 'react-player'
+import React from 'react'
 import { Inter } from '@next/font/google'
-import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    return (
+        <div className="flex flex-col justify-center items-center   py-10">
+            <div className="flex gap-10">
+                <Image src="/lion.png" alt="lion" width={100} height={100} />
+                <h1 className=" relative  text-4xl py-10 inter.className">
+                    Lion and Lamb
+                </h1>
+                <Image src="/sheep.png" alt="sheep" width={100} height={100} />
+            </div>
+            <div>
+                <audio controls autoPlay id="audio">
+                    <source src="prey.mp3" />
+                    Your browser does not support the <code>audio</code>{' '}
+                    element.
+                </audio>
+            </div>
+            <section className="py-10 ">
+                <p>
+                    It began with a game, all just in fun, <br />
+                    Who knew that my heart would so soon succumb, indeed a new
+                    love had begun! <br />
+                    With each roll of the dice, every robber&apos;s move, <br />
+                    You took hold of my heart, I had everything to prove,
+                </p>
+                <br />
+                <p>
+                    Lion and lamb, predator and prey, <br />
+                    Such love seems impossible in every way, <br />
+                    The lion roars, proud and courageous he may seem, <br />
+                    Little did he know, his appearance seemed extreme. <br />
+                </p>
+                <br />
+
+                <p>
+                    The lamb, so gentle, seeks true love divine, <br />
+                    At first, she saw flaws, she couldn&apos;t entwine <br />
+                    His teeth were too sharp, his roar was too loud <br />
+                    She didn&apos;t even care that he was well endowed, <br />
+                </p>
+                <br />
+
+                <p>
+                    But soon time went by, her perspective did shift, <br />
+                    She realized his strength was a gift to uplift. <br />
+                    His courage was freedom, they could do so much more, <br />
+                    His love was a door, waiting to be opened with treasure in
+                    store. <br />
+                </p>
+                <br />
+
+                <p>
+                    Her heart opened wide, a warmth filled her soul, <br />
+                    Her kindness the catalyst, her light he extolled. <br />
+                    She couldn&apos;t resist, she had finally found, <br />
+                    The one she truly missed, her heart beat such a resounding
+                    sound. <br />
+                </p>
+                <br />
+
+                <p>
+                    The lion, once fierce, now purrs with her touch, <br />
+                    Her love was his cure, grateful he was, for so much! <br />
+                    Was he still a hunter, she still his prey? <br />
+                    No! A bond formed by love, stronger than any force at play.
+                </p>
+                <br />
+
+                <p>
+                    Their steps in perfect rhythm, a dance of love, <br />
+                    The lion leaps forward, four left paws from above, <br />
+                    The lamb, carefree and graceful, prances with ease, <br />A
+                    reminder that love knows no bounds and can conquer all
+                    beasts.
+                </p>
+                <br />
+
+                <p>
+                    A lion and a lamb, forever in chase No longer predator, no
+                    longer prey, <br />
+                    Together they roamed, in an endless embrace.
+                </p>
+            </section>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    )
 }
